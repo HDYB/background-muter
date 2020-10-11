5c60962eb60ea7a74ddd26ddf7981fb6f2f7fb69
HDYB <71931720+HDYB@users.noreply.github.com>
2020-10-11 18:48:48 +0300

    Setting up things

---
+++ src/main/java/net/background/muter/BackgroundMuter.java
@@ -0,0 +1,11 @@
+package net.background.muter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BackgroundMuter implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
